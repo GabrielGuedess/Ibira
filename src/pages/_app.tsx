c194aa1ba7fc7cfcba58223b75bef9dbde912a08
Rafael <rafaabatistas@gmail.com>
2021-05-04 08:26:10 -0300

    Change title Head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Head>
-        <title>Boilerplate</title>
+        <title>Ibira</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
