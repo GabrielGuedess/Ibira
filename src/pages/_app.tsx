8f240c881d8fab6fd41a6b681261194cd8ff5d2b
Rafael <rafaabatistas@gmail.com>
2021-05-04 08:32:36 -0300

    Add shortcut icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,7 @@ function App({ Component, pageProps }: AppProps) {
     <ThemeProvider theme={theme}>
       <Head>
         <title>Ibira</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="shortcut icon" href="/img/shortcut-icon.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092B" />
