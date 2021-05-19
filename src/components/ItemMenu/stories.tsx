b64c1a259a7bebb93215dcd4d31294c8e60c3135
Gabriel Guedes <gabrielrguedess@gmail.com>
2021-05-19 19:56:30 -0300

    Add props in storyBook

--- src/components/ItemMenu/stories.tsx
+++ src/components/ItemMenu/stories.tsx
@@ -1,9 +1,10 @@
 import { Story, Meta } from '@storybook/react/types-6-0';
 import ItemMenu from '.';
+import categories from './categories';
 
 export default {
   title: 'ItemMenu',
   component: ItemMenu,
 } as Meta;
 
-export const Default: Story = () => <ItemMenu />;
+export const Default: Story = () => <ItemMenu categories={categories} />;
