6eead4d2b178023a365877ec3d3e867cf641ba4b
Gabriel Guedes <gabrielrguedess@gmail.com>
2021-05-19 20:01:50 -0300

    Fix props logo

--- src/components/Logo/stories.tsx
+++ src/components/Logo/stories.tsx
@@ -1,9 +1,9 @@
 import { Story, Meta } from '@storybook/react/types-6-0';
-import Logo, { LogoProps } from '.';
+import Logo from '.';
 
 export default {
   title: 'Logo',
   component: Logo,
 } as Meta;
 
-export const Default: Story<LogoProps> = args => <Logo {...args} />;
+export const Default: Story = () => <Logo />;
