56d51ae184b5eaf5793d69f6322afc743f8bf810
Rafael Batista <rafaabatistas@gmail.com>
2021-05-28 14:31:23 -0300

    Create getStaticProps function for API request

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,3 +3,26 @@ import NavBar from 'components/NavBar';
 export default function Home() {
   return <NavBar />;
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('produtos', {
+    params: {
+      _limit: 12,
+      _order: 'desc',
+    },
+  });
+
+  const produtos = data.map(produto => ({
+    id: produto.id_produto,
+    nome: produto.nome,
+    preco: produto.preco,
+    marca: produto.marca,
+    categoria: produto.categoria,
+  }));
+
+  return {
+    props: {
+      produtos,
+    },
+  };
+};
