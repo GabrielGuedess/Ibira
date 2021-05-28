fb6d2bfce895a192ab7c2b6252526fb27de37c22
Rafael Batista <rafaabatistas@gmail.com>
2021-05-28 14:31:23 -0300

    Create getStaticProps function for API request

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,32 @@
-// import client from 'graphql/client';
-// import GET_CATEGORIES from 'graphql/queries/getCategories';
-// import { GetStaticProps } from 'next';
+import { GetStaticProps } from 'next';
 
 import Home from 'templates/Home';
-// import { CategoriesProps } from 'types/api';
+
+import { api } from '../services/api';
 
 export default function Index() {
   return <Home />;
 }
 
-// export const getStaticProps: GetStaticProps = async () => {
-//   const { categories, burgerimg } = await client.request(GET_CATEGORIES);
-//   return {
-//     props: {
-//       categories,
-//       burgerimg,
-//     },
-//   };
-// };
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('produtos', {
+    params: {
+      _limit: 12,
+      _order: 'desc',
+    },
+  });
+
+  const produtos = data.map(produto => ({
+    id: produto.id_produto,
+    nome: produto.nome,
+    preco: produto.preco,
+    marca: produto.marca,
+    categoria: produto.categoria,
+  }));
+
+  return {
+    props: {
+      produtos,
+    },
+  };
+};
