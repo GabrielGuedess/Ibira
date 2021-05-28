e7712a9749ac47d892296b9d1bcaa581ac889575
Rafael Batista <rafaabatistas@gmail.com>
2021-05-28 14:33:16 -0300

    Update the context when the query is made and the page is loaded

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,15 @@
 import NavBar from 'components/NavBar';
 
-export default function Home() {
+import { useContext } from 'react';
+import { GetStaticProps } from 'next';
+import { ProdutoContext } from '../contexts/ProdutoContext';
+import { api } from '../services/api';
+
+import type { ProdutosProps } from '../types/produtos';
+
+export default function Home({ produtos }: ProdutosProps) {
+  const { setProdutoList } = useContext(ProdutoContext);
+  setProdutoList(produtos);
   return <NavBar />;
 }
 
