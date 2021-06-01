225b4305b5ecd23507ee7babc6682437b30cc39c
Rafael Batista <rafaabatistas@gmail.com>
2021-06-01 16:22:29 -0300

    Expanding the context area

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,23 +8,23 @@ import ContextProviders from '../contexts/contextUtils/providers';
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Head>
-        <title>Ibira</title>
-        <link rel="shortcut icon" href="/img/shortcut-icon.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#06092B" />
-        <meta
-          name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
-        />
-      </Head>
-      <GlobalStyles />
-      <ContextProviders>
+    <ContextProviders>
+      <ThemeProvider theme={theme}>
+        <Head>
+          <title>Ibira</title>
+          <link rel="shortcut icon" href="/img/shortcut-icon.png" />
+          <link rel="apple-touch-icon" href="/img/icon-512.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="theme-color" content="#06092B" />
+          <meta
+            name="description"
+            content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          />
+        </Head>
+        <GlobalStyles />
         <Component {...pageProps} />
-      </ContextProviders>
-    </ThemeProvider>
+      </ThemeProvider>
+    </ContextProviders>
   );
 }
 
