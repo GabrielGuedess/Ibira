f39a907caf14169bf3f4d27951df4b556eb81951
Rafael Batista <rafaabatistas@gmail.com>
2021-06-01 16:28:10 -0300

    Add SetMenuContext in Providers

--- src/contexts/contextUtils/providers.tsx
+++ src/contexts/contextUtils/providers.tsx
@@ -1,9 +1,12 @@
 import ProdutoContextProvider from '../ProdutoContext';
+import SetMenuContextProvider from '../SetMenuContext';
 
 import type { ProdutoProps } from '../ProdutoContext';
 
 const Providers = ({ children }: ProdutoProps) => (
-  <ProdutoContextProvider>{children}</ProdutoContextProvider>
+  <ProdutoContextProvider>
+    <SetMenuContextProvider>{children}</SetMenuContextProvider>
+  </ProdutoContextProvider>
 );
 
 export default Providers;
